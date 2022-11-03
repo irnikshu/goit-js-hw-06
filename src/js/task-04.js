6cf2a51f7902a0f96ae0e12c300642f49b8a4391
irnikshu <95768966+irnikshu@users.noreply.github.com>
2022-11-03 11:46:19 +0200

    Updata task 4

--- src/js/task-04.js
+++ src/js/task-04.js
@@ -1,17 +1,26 @@
 const incrementBtn = document.querySelector("[data-action='increment']");
+// const incrementBtn = document.querySelector('increment');
 const decrementBtn = document.querySelector("[data-action='decrement']");
+// const decrementBtn = document.querySelector('decrement');
 // const value = document.querySelector('#value');
 // const wrapper = document.querySelector('#counter');
 
 let counterValue = 0;
-const increment = () => {
+// const increment = () => {
+//     counterValue += 1;
+
+//     value.textContent = counterValue;
+// };
+// const decrement = () => {
+//     counterValue -= 1;
+//     value.textContent = counterValue;
+// };
+incrementBtn.addEventListener('click', increment => {
     counterValue += 1;
 
     value.textContent = counterValue;
-};
-const decrement = () => {
+});
+decrementBtn.addEventListener('click', decrement => {
     counterValue -= 1;
     value.textContent = counterValue;
-};
-incrementBtn.addEventListener('click', increment);
-decrementBtn.addEventListener('click', decrement);
+});
