1c6a485558aee2121cb9ab6a92d4f0038239f403
irnikshu <95768966+irnikshu@users.noreply.github.com>
2022-11-04 17:56:05 +0200

    Task 8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,30 @@
+const form = document.querySelector('.login-form');
+const inputs = document.querySelectorAll('input');
 
+form.addEventListener('submit', onFormSubmit);
+function onFormSubmit(event) {
+    event.preventDefault();
+    // const formElements = event.currentTarget.elements;
+    // const mail = formElements.email.value;
+    // const pasword = formElements.password.value;
+    // const formData = {
+    //     mail,
+    //     pasword,
+    // }
+    // console.log(formData);
+    inputs.forEach(input => {
+        if (input.value.trim() === '') {
+            alert("Все поля должны быть заполнены")
+        }
+    });
+    
+    const formData = new FormData(event.currentTarget);
+    formData.forEach((value, name) => {
+        console.log('onFormSubmit -> name', name);
+        console.log('onFormSubmit -> value', value);
+    });
+    
+  
+    // document.getElementById('login-form').reset();
+    event.target.reset();
+}
