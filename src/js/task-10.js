3381cc751e604d4330b5a351b2cd10ed49375eb2
irnikshu <95768966+irnikshu@users.noreply.github.com>
2022-11-06 23:06:01 +0200

    Task 10

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -1,3 +1,36 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const createBtn = document.querySelector("[data-create]");
+const destroyBtn = document.querySelector("[data-destroy]");
+const inputEl = document.querySelector('.controls');
+
+
+
+createBtn.addEventListener('click', createBoxes);
+destroyBtn.addEventListener('click', destroyBoxes);
+// inputEl.addEventListener('input', onInputNumber);
+
+
+function onInputNumber() {
+  const amount = inputEl.currentTarget.value;
+  createBoxes(amount);
+}
+
+function createBoxes(amount) {
+  const basicSize = 30;
+ 
+  for (let i = 0; i < amount; i+=1) {
+    const size = basicSize + i * 10;
+    const div = document.createElement("div");
+    div.style.cssText = `width: ${size}px; height: ${size}px; background-color: ${getRandomHexColor()}`;
+    
+    boxes.appendChild(div);
+  }
+  
+}
+
+function destroyBoxes() {
+  boxes.textContent = '';
+}
+
