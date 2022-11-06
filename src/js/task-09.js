b2aafb69207dba27bdfd4ccc091ef2b41aeeb0df
irnikshu <95768966+irnikshu@users.noreply.github.com>
2022-11-06 20:07:17 +0200

    Update task 9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,20 +1,12 @@
-const colorBtn = document.querySelector('.change-color');
-const spanEl = document.querySelectorAll('.color');
-const bodyEl = document.querySelector('body');
 
+const body = document.body;
+const colorBtn = body.querySelector(".change-color");
+const spanEL = body.querySelector(".color");
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
-
-
-colorBtn.addEventListener('click', onBodyColorChange);
-
+colorBtn.addEventListener("click", onBodyColorChange);
 function onBodyColorChange(event) {
-  function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
-  // const color = getRandomHexColor();
-  bodyEl.style.backgroundColor = getRandomHexColor();
-  spanEl.textContent = getRandomHexColor();
-  
+  body.style.backgroundColor = getRandomHexColor();
+  spanEL.textContent = body.style.backgroundColor;
 }
