f127e733b454e98e39d67cfc2e5d6a277d7ca469
irnikshu <95768966+irnikshu@users.noreply.github.com>
2022-11-09 00:18:19 +0200

    Update task 2

--- js/task-02.js
+++ js/task-02.js
@@ -8,12 +8,18 @@ const ingredients = [
 ];
 const ingredientsEl = document.querySelector("#ingredients");
 
-for (const obj of ingredients) {
-  const element = document.createElement("li");
-  element.textContent = obj;
-  element.classList.add("item");
- ingredientsEl.append(element);
-};
+// for (const obj of ingredients) {
+//   const element = document.createElement("li");
+//   element.textContent = obj;
+//   element.classList.add("item");
+// //  ingredientsEl.append(element);
+// };
 
-
-
+function makeLiEl(ingredient) {
+  const liEl = document.createElement("li");
+  liEl.textContent = ingredient;
+  liEl.classList.add("item");
+  return liEl;
+}
+const elements = ingredients.map(makeLiEl);
+ingredientsEl.append(...elements);
